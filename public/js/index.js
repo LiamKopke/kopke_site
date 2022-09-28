0ec377949bac06cb00d1c7a353434b6b45b05a5e
LiamKopke <78220160+LiamKopke@users.noreply.github.com>
2022-09-28 15:38:33 -0400

    reset css and split css into different files

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,1 @@
-// Make the footer relative once containerMain is at the bottom of the page
-// This is to make sure the footer is at the bottom of the page
-window.onscroll = function (ev) {
-	if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-		document.getElementById("footer").style.position = "relative";
-	} else {
-		document.getElementById("footer").style.position = "fixed";
-	}
-};
+
--- public/js/theme.js
+++ public/js/theme.js
@@ -1,57 +1,75 @@
-const darkMode = document.querySelector("#dark-mode");
+const theme = document.querySelector("#theme");
 const modeLogo = document.querySelector("#mode-logo");
 const modeText = document.querySelector("#mode-text");
+const dropdown = document.querySelector(".dropdown-content");
+const darkMode = document.querySelector("#dark-mode");
+const lightMode = document.querySelector("#light-mode");
+const osDefault = document.querySelector("#os-default");
 
 function setColorScheme(scheme) {
 	switch (scheme) {
 		case "dark":
 			document.body.classList.remove("light-mode-variables");
-			modeLogo.innerHTML = "light_mode";
-			modeText.innerHTML = "Light Mode";
+			modeLogo.innerHTML = "dark_mode";
+			modeText.innerHTML = "Dark Mode";
 			window.localStorage.setItem("isDark", true);
 			break;
 		case "light":
-			document.body.classList.remove("light-mode-variables");
 			document.body.classList.add("light-mode-variables");
-			console.log("added light mode");
-			modeLogo.innerHTML = "mode_night";
-			modeText.innerHTML = "Dark Mode";
+			modeLogo.innerHTML = "light_mode";
+			modeText.innerHTML = "Light Mode";
 			window.localStorage.setItem("isDark", false);
 			break;
-		default:
+		case "os-dark":
 			document.body.classList.remove("light-mode-variables");
 			modeLogo.innerHTML = "contrast";
 			modeText.innerHTML = "OS Default";
-			window.localStorage.setItem("isDark", true);
+			window.localStorage.setItem("isDark", null);
+			break;
+		case "os-light":
+			document.body.classList.add("light-mode-variables");
+			modeLogo.innerHTML = "contrast";
+			modeText.innerHTML = "OS Default";
+			window.localStorage.setItem("isDark", null);
 			break;
 	}
+	dropdown.classList.add("hide");
 }
 
 function getPreferredColorScheme() {
 	if (window.matchMedia) {
 		if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-			return "dark";
+			return "os-dark";
 		} else {
-			return "light";
+			return "os-light";
 		}
 	}
 	return "dark";
 }
 
-function getSetColorScheme() {
-	if (window.localStorage.getItem("isDark") === null) {
-		setColorScheme(getPreferredColorScheme());
-	} else if (window.localStorage.getItem("isDark") === "true") {
-		setColorScheme("dark");
-	} else if (window.localStorage.getItem("isDark") === "false") {
-		setColorScheme("light");
-	}
-}
+// Event Listeners
 
 if (window.matchMedia) {
 	var colorSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
-	colorSchemeQuery.addEventListener(
-		"change",
-		setColorScheme(getPreferredColorScheme())
-	);
+	colorSchemeQuery.addEventListener("change", () => {
+		if (modeText.innerHTML === "OS Default") {
+			setColorScheme(getPreferredColorScheme());
+		}
+	});
 }
+
+theme.addEventListener("click", function () {
+	dropdown.classList.toggle("hide");
+});
+
+darkMode.addEventListener("click", function () {
+	setColorScheme("dark");
+});
+
+lightMode.addEventListener("click", function () {
+	setColorScheme("light");
+});
+
+osDefault.addEventListener("click", function () {
+	setColorScheme(getPreferredColorScheme());
+});
